9f5924176d549dc2f09c813a929ae9a2a2922ceb
Berlin Ortega <marlitros200002@gmail.com>
2025-05-28 17:33:31 -0600

    Protección de rutas

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,6 +1,13 @@
 import { Outlet } from "react-router-dom"
+import { userAuth } from "@/hooks/userAuth";
+import { Navigate } from "react-router-dom";
 
 export default function AppLayout() {
+    const { data, isLoading, isError } = userAuth();
+
+    if(isLoading) return <p>Cargando...</p>
+    if(isError) return <Navigate to="/auth" replace/>
+    
     return (
         <>
             <div>Hi App layout</div>
